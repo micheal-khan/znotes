66fe2389e496286d6280b31553e26cc3bca3b76c
Micheal <khanzaidan786@gmail.com>
2025-10-27 15:52:22 +0530

    favicon done

--- app/layout.tsx
+++ app/layout.tsx
@@ -16,7 +16,7 @@ const geistMono = Geist_Mono({
 });
 
 export const metadata: Metadata = {
-  title: "Znotes",
+  title: "Znotes - Notes taking app by Micheal Khan",
   description: "Notes taking app by Micheal Khan",
 };
 
